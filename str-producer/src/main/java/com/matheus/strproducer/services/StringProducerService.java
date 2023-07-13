2e6ce4013f45af1257c2578fd29ac857bac95c3a
Matheus Vieira <contatomatheus959@gmail.com>
2023-07-13 10:13:47 -0300

    adicionado o callback and log

--- str-producer/src/main/java/com/matheus/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/matheus/strproducer/services/StringProducerService.java
@@ -1,17 +1,33 @@
 package com.matheus.strproducer.services;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class StringProducerService {
-	
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
-	
-	public void sendMessage(String message) {
-		kafkaTemplate.send("str-topic",message);
-	}
 
+    private static final Logger log = LogManager.getLogger(StringProducerService.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                	if(success != null) {
+                		log.info("Mensagem enviada com sucesso {}", message);
+                		log.info("partition {}, offset{}", 
+                				success.getRecordMetadata().partition(),
+                				success.getRecordMetadata().offset()
+                				);
+                		}
+                },
+                error -> log.error("Falha no envio da mensagem")
+        );
+    }
 }
