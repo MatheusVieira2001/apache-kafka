1523397eeb2c32b04c5c34276a7f13bf51a7ff29
Matheus Vieira <contatomatheus959@gmail.com>
2023-07-13 16:32:53 -0300

    adicionado ConsumerCustom

---
+++ str-consumer/src/main/java/com/matheus/strconsumer/listeners/StrConsumerCustomListener.java
@@ -0,0 +1,36 @@
+package com.matheus.strconsumer.listeners;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface StrConsumerCustomListener {
+
+	@AliasFor(annotation = KafkaListener.class, attribute = "topics") 
+	String[] topics() default "str-topic";
+	
+	@AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
+	String containerFactory() default "strContainerFactory";
+	
+	@AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+	String groupId() default "";
+	
+	/*
+	 * GRUPOS DE CONSUMOS DIFERENTES PODEM processar A MENSAGEM IGUALMENTE (DEPENDENTE DAS PARTIÇÕES)!
+	 * Neste exemplo temos um produtor com 2 partiçoes e estas irão ser consumidas igualmente por ambos os grupos 
+	 * group-1: partitions assigned: [str-topic-0, str-topic-1] visualizará a particao 0 e 1
+	 * group-2: partitions assigned: [str-topic-0, str-topic-1]
+	 * caso possua 2 metodos para o mesmo grupo as partições serão divididas exemplo
+	 * group-1: partitions assigned: [str-topic-1] - metodo 1
+	 * group-1: partitions assigned: [str-topic-0] - metodo 2
+	 *também é possivel definir qual  a partição deseja que o listener atue
+	 *topicPartitions = {@TopicPartition(topic = "str-topic", partitions = {"0"})}, nesse caso verifica a 0 ou o numero desejado de acordo com as partições existentes
+	*/
+}
--- str-consumer/src/main/java/com/matheus/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/matheus/strconsumer/listeners/StrConsumerListener.java
@@ -2,30 +2,26 @@ package com.matheus.strconsumer.listeners;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StrConsumerListener {
 
-	  private static final Logger log = LogManager.getLogger(StrConsumerListener.class);
+	private static final Logger log = LogManager.getLogger(StrConsumerListener.class);
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void listener(String message) {
-		log.info("CREATE ::: Mensagem recebida {}",message);
+		log.info("LISTENER (0) ::: Mensagem recebida {}",message);
 	}
-	/*
-	 * GRUPOS DE CONSUMOS DIFERENTES PODEM processar A MENSAGEM IGUALMENTE (DEPENDENTE DAS PARTIÇÕES)!
-	 * Neste exemplo temos um produtor com 2 partiçoes e estas irão ser consumidas igualmente por ambos os grupos 
-	 * group-1: partitions assigned: [str-topic-0, str-topic-1] visualizará a particao 0 e 1
-	 * group-2: partitions assigned: [str-topic-0, str-topic-1]
-	 * caso possua 2 metodos para o mesmo grupo as partições serão divididas exemplo
-	 * group-1: partitions assigned: [str-topic-1] - metodo 1
-	 * group-1: partitions assigned: [str-topic-0] - metodo 2
-	*/
-	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+	
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
-		log.info("CREATE ::: Mensagem recebida {}",message);
+		log.info("CREATE (1) ::: Mensagem recebida {}",message);
+	}
+	
+	@StrConsumerCustomListener(groupId = "group-2")
+	public void history(String message) {
+		log.info("HISTORY (0-1) ::: Mensagem recebida {}",message);
 	}
 	
 }
